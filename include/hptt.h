b7dbe8ec3e9372826344eb716cfde60baa815ec6
Atte Torri <atte.torri@universite-paris-saclay.fr>
2024-03-28 18:11:15 +0100

    ignore -Wc99-extension warnings for _Complex

--- include/hptt.h
+++ include/hptt.h
@@ -301,6 +301,9 @@ void dTensorTranspose( const int *perm, const int dim,
                  const double beta,        double *B,                   const int *outerSizeB, 
                  const int numThreads, const int useRowMajor = 0);
 
+// Avoid warning from clang for _Complex
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wc99-extensions"
 void cTensorTranspose( const int *perm, const int dim,
                  const float _Complex alpha, bool conjA, const float _Complex *A, const int *sizeA, const int *outerSizeA, 
                  const float _Complex beta,                    float _Complex *B,                   const int *outerSizeB, 
@@ -310,4 +313,5 @@ void zTensorTranspose( const int *perm, const int dim,
                  const double _Complex alpha, bool conjA, const double _Complex *A, const int *sizeA, const int *outerSizeA, 
                  const double _Complex beta,                    double _Complex *B,                   const int *outerSizeB, 
                  const int numThreads, const int useRowMajor = 0);
+#pragma clang diagnostic pop
 }
